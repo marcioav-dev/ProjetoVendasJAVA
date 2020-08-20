b3d4f539c279d3686d04a228eadf3368e1390378
Marcio Vieira <marcioav.dev@gmail.com>
2020-08-20 17:59:54 -0300

    Projeto Criado

---
+++ src/main/java/com/br11/sistemavendas/SistemavendasApplication.java
@@ -0,0 +1,13 @@
+package com.br11.sistemavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemavendasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br11/sistemavendas/SistemavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br11.sistemavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemavendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
